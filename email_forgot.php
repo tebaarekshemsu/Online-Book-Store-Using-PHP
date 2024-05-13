4e166320bf163891708c5d1ac85a8030b17864d4
Tebarek Shemsu <130053060+tebaarekshemsu@users.noreply.github.com>
2024-05-13 13:07:46 +0300

    Update email_forgot.php

--- email_forgot.php
+++ email_forgot.php
@@ -22,7 +22,7 @@ function send_mail($recipient, $subject, $message)
     $mail->Port = 587;
     $mail->Username = 'tamedemse83@gmail.com';
     $mail->Password = 'ebgo oevx esdj tffo';
-    $mail->setFrom('your-email@gmail.com', 'Your Sender Name');
+    $mail->setFrom('your-email@gmail.com', 'Online Book Store');
     $mail->addAddress($recipient);
     $mail->isHTML(true);
     $mail->Subject = $subject;
