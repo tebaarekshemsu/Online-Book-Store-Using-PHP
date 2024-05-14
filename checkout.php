b5b838d84db42474a9822c6c1184a945054637a2
Tebarek Shemsu <130053060+tebaarekshemsu@users.noreply.github.com>
2024-05-14 20:38:02 +0300

    some minor chsnges

--- checkout.php
+++ checkout.php
@@ -1,3 +1,4 @@
+Tamrat, [5/14/2024 12:48 PM]
 <?php
 include 'config.php';
 
@@ -28,7 +29,7 @@ if (isset($_POST['order_btn'])) {
    $cart_total = 0;
    $cart_quantity[] = '';
 
-   $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+   $cart_query = mysqli_query($conn, "SELECT * FROM cart WHERE user_id = '$user_id'") or die('query failed');
    if (mysqli_num_rows($cart_query) > 0) {
       while ($cart_item = mysqli_fetch_assoc($cart_query)) {
          $cart_quantity[] = $cart_item['name'] . ' (' . $cart_item['quantity'] . ') ';
@@ -39,7 +40,7 @@ if (isset($_POST['order_btn'])) {
 
    $total_products = implode(', ', $cart_quantity);
 
-   $order_query = mysqli_query($conn, "SELECT * FROM `orders` WHERE name = '$name' AND number = '$number' AND email = '$email' AND method = '$method' AND total_products = '$total_products' AND total_price = '$cart_total'") or die('query failed');
+   $order_query = mysqli_query($conn, "SELECT * FROM orders WHERE name = '$name' AND number = '$number' AND email = '$email' AND method = '$method' AND total_products = '$total_products' AND total_price = '$cart_total'") or die('query failed');
 
    if ($cart_total == 0) {
       $message = 'Your cart is empty';
@@ -47,9 +48,9 @@ if (isset($_POST['order_btn'])) {
       if (mysqli_num_rows($order_query) > 0) {
          $message = 'Order already placed!';
       } else {
-         mysqli_query($conn, "INSERT INTO `orders`(user_id, name, number, email, method, total_products, total_price,payment_status, latitude, longitude) VALUES('$user_id', '$name', '$number', '$email', '$method', '$total_products', '$cart_total', 'pending', '$latitude', '$longitude')") or die('query failed');
+         mysqli_query($conn, "INSERT INTO orders(user_id, name, number, email, method, total_products, total_price,payment_status, latitude, longitude) VALUES('$user_id', '$name', '$number', '$email', '$method', '$total_products', '$cart_total', 'pending', '$latitude', '$longitude')") or die('query failed');
          $message = 'Order placed successfully!';
-         mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+         mysqli_query($conn, "DELETE FROM cart WHERE user_id = '$user_id'") or die('query failed');
          header('location: shop.php');
          exit;
       }
@@ -80,7 +81,8 @@ if (isset($_POST['order_btn'])) {
       <p><a href="home.php">Home</a> / Checkout </p>
    </div>
 
-   <section class="checkout">
+Tamrat, [5/14/2024 12:48 PM]
+<section class="checkout">
       <form method="post" action="">
          <div class="inputBox">
             <span>Your Number :</span>
