1da2dcddc1c23375e080213fa79caddc5f524865
Tebarek Shemsu <130053060+tebaarekshemsu@users.noreply.github.com>
2024-05-14 11:50:55 +0300

    add some functionality

--- shop.php
+++ shop.php
@@ -1,5 +1,4 @@
 <?php
-
 include 'config.php';
 
 session_start();
@@ -17,18 +16,30 @@ if (isset($_POST['add_to_cart'])) {
    $product_image = $_POST['product_image'];
    $product_quantity = $_POST['product_quantity'];
 
-   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+   // Fetch available piece from the database
+   $fetch_product = mysqli_query($conn, "SELECT pieces FROM `products` WHERE name = '$product_name'") or die('query failed');
+   $product_data = mysqli_fetch_assoc($fetch_product);
+   $pieces = $product_data['pieces'];
 
-   if (mysqli_num_rows($check_cart_numbers) > 0) {
-      $message[] = 'already added to cart!';
+   if ($product_quantity > $pieces) {
+      $message[] = 'Sorry, only ' . $pieces . ' piece(s) available!';
    } else {
-      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
-      $message[] = 'product added to cart!';
+      $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+      if (mysqli_num_rows($check_cart_numbers) > 0) {
+         $message[] = 'Already added to cart!';
+      } else {
+         // Decrease the available piece in the products table
+         $new_pieces = $pieces - $product_quantity;
+         mysqli_query($conn, "UPDATE `products` SET pieces = '$new_pieces' WHERE name = '$product_name'") or die('query failed');
+
+         mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+         $message[] = 'Product added to cart!';
+      }
    }
 }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -37,7 +48,7 @@ if (isset($_POST['add_to_cart'])) {
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>shop</title>
+   <title>Shop</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -55,7 +66,7 @@ if (isset($_POST['add_to_cart'])) {
 
 
    <div class="headingshop">
-      <h3>our shop</h3>
+      <h3>Our Shop</h3>
       <p> <a href="home.php">Home</a> / Shop </p>
    </div>
 
@@ -74,16 +85,17 @@ if (isset($_POST['add_to_cart'])) {
                   <img class="image" src="<?php echo $fetch_products['image']; ?>" alt="">
                   <div class="name"><?php echo $fetch_products['name']; ?></div>
                   <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
-                  <input type="number" min="1" name="product_quantity" value="1" class="qty">
+                  <!-- Fetch maximum piece value from the database -->
+                  <input type="number" min="1" max="<?php echo $fetch_products['pieces']; ?>" name="product_quantity" value="1" class="qty">
                   <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
                   <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
                   <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-                  <input type="submit" value="add to cart" name="add_to_cart" class="btne">
+                  <input type="submit" value="Add to Cart" name="add_to_cart" class="btne">
                </form>
          <?php
             }
          } else {
-            echo '<p class="empty">no products added yet!</p>';
+            echo '<p class="empty">No products added yet!</p>';
          }
          ?>
       </div>
